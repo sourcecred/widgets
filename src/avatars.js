4c210df056b18c6606595c96bf37a564e1825e60
Robin IT <info@robin-it.com>
2019-07-12 22:30:25 +0200

    Initial contributor wall experiment

---
+++ src/avatars.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import getAvatarURL from 'gh-avatar'
+import axios from 'axios'
+import sharp from 'sharp'
+
+export const fetchEmbedableAvatar = async (username, token, {avatarSize}) => {
+	const url = await getAvatarURL(username);
+	const httpResult = await axios.get(url, {responseType: 'arraybuffer'});
+	const resizedImage = await
+		sharp(Buffer.from(httpResult.data))
+		.resize(avatarSize, avatarSize)
+		.png()
+		.toBuffer();
+
+	return `data:image/png;base64,${resizedImage.toString('base64')}`;
+};
