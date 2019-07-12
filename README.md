5f873e647d7c30a06623135c7e66888e6f986974
Robin IT <info@robin-it.com>
2019-07-12 22:30:25 +0200

    Initial contributor wall experiment

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 A Node.js based set of tools to interpret SourceCred scores and generate widgets.
 For example to use in README's or as webpage static assets.
+
+## Example usage
+
+```sh
+# 1. Use the main sourcecred's score feature to export scores to a file.
+export SOURCECRED_GITHUB_TOKEN=YOUR_GITHUB_TOKEN
+node PATH_TO_SOURCECRED/bin/sourcecred.js load sourcecred/sourcecred
+node PATH_TO_SOURCECRED/bin/sourcecred.js scores sourcecred/sourcecred > scores.json
+
+# 2. Install this package's dependencies.
+yarn
+
+# 3. Generate a contributor wall SVG.
+./bin/contributor-wall-svg.js < scores.json > contributors.svg
+```
---
+++ src/svg.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import {fetchEmbedableAvatar} from './avatars'
+
+export const createContributorWall = async (users, token, {minCred, maxUsers, usersPerRow, avatarSize, margin}) => {
+	const selectedUsers = users
+		.slice(0, maxUsers)
+		.filter(user => user.totalCred >= minCred);
+
+	const usersWithImages = await Promise.all(
+		selectedUsers.map(user =>
+			fetchEmbedableAvatar(user.id, token, {avatarSize})
+			.then(avatarSlug => ({
+				id: user.id,
+				totalCred: user.totalCred,
+				avatarSlug
+			}))
+		)
+	);
+
+	const images = usersWithImages
+		.map((user, i) => ({
+			...user,
+			posX: (i % usersPerRow) * (avatarSize + margin),
+			posY: Math.floor(i / usersPerRow) * (avatarSize + margin)
+		}))
+		.map(user => 
+			`<a href="https://github.com/${user.id}" class="link" target="_blank" id="${user.id}">` +
+			`<image x="${user.posX}" y="${user.posY}" width="${avatarSize}" height="${avatarSize}" xlink:href="${user.avatarSlug}"/>` +
+			`</a>`
+		);
+
+	return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${(avatarSize + margin) * usersPerRow - margin}" height="${Math.ceil(images.length / usersPerRow) * (avatarSize + margin) - margin}">
+		<style>.link { cursor: pointer; }</style>
+		${images.join('\n')}
+	</svg>`
+};
