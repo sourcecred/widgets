526c1b8ef079e319407a030d5992f0e4b362dc17
Dandelion Mané <decentralion@dandelion.io>
2019-07-16 02:02:36 +0100

    Add prettier (and reformat code)
    This commit adds a dependency on prettier, and sets up scripts in
    `package.json` to use prettify to format all the files.
    
    I then re-formatted all the files using `yarn prettify`.
    
    Test plan: `yarn prettify` produces no changes. `yarn check-pretty`
    exits without issue.

--- src/scores.js
+++ src/scores.js
@@ -1,19 +1,24 @@
-'use strict';
+"use strict";
 
-const EXPECTED_SCORE_VERSION = '0.1.0';
-const EXPECTED_SCORE_TYPE = 'sourcecred/cli/scores';
+const EXPECTED_SCORE_VERSION = "0.1.0";
+const EXPECTED_SCORE_TYPE = "sourcecred/cli/scores";
 
-const validateScoreFormat = plainObject => {
-	const [versionHeader, scoreData] = plainObject;
+const validateScoreFormat = (plainObject) => {
+  const [versionHeader, scoreData] = plainObject;
 
-	if(versionHeader.version !== EXPECTED_SCORE_VERSION || versionHeader.type !== EXPECTED_SCORE_TYPE) {
-		throw new Error(`Expecting type "${EXPECTED_SCORE_TYPE}" and version "${EXPECTED_SCORE_VERSION}" in the header of our scores file.`);
-	}
+  if (
+    versionHeader.version !== EXPECTED_SCORE_VERSION ||
+    versionHeader.type !== EXPECTED_SCORE_TYPE
+  ) {
+    throw new Error(
+      `Expecting type "${EXPECTED_SCORE_TYPE}" and version "${EXPECTED_SCORE_VERSION}" in the header of our scores file.`
+    );
+  }
 
-	return scoreData;
+  return scoreData;
 };
 
-export const fromJSONString = jsonString => {
-	const plainObject = JSON.parse(jsonString);
-	return validateScoreFormat(plainObject);
+export const fromJSONString = (jsonString) => {
+  const plainObject = JSON.parse(jsonString);
+  return validateScoreFormat(plainObject);
 };
