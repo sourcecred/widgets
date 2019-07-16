b63804bb233b1b6e96dadd779a60111db7bf3ae2
Dandelion Mané <decentralion@dandelion.io>
2019-07-16 02:02:36 +0100

    Add prettier (and reformat code)
    This commit adds a dependency on prettier, and sets up scripts in
    `package.json` to use prettify to format all the files.
    
    I then re-formatted all the files using `yarn prettify`.
    
    Test plan: `yarn prettify` produces no changes. `yarn check-pretty`
    exits without issue.

--- src/avatars.js
+++ src/avatars.js
@@ -1,17 +1,16 @@
-'use strict';
+"use strict";
 
-import getAvatarURL from 'gh-avatar'
-import axios from 'axios'
-import sharp from 'sharp'
+import getAvatarURL from "gh-avatar";
+import axios from "axios";
+import sharp from "sharp";
 
 export const fetchEmbedableAvatar = async (username, token, {avatarSize}) => {
-	const url = await getAvatarURL(username);
-	const httpResult = await axios.get(url, {responseType: 'arraybuffer'});
-	const resizedImage = await
-		sharp(Buffer.from(httpResult.data))
-		.resize(avatarSize, avatarSize)
-		.png()
-		.toBuffer();
+  const url = await getAvatarURL(username);
+  const httpResult = await axios.get(url, {responseType: "arraybuffer"});
+  const resizedImage = await sharp(Buffer.from(httpResult.data))
+    .resize(avatarSize, avatarSize)
+    .png()
+    .toBuffer();
 
-	return `data:image/png;base64,${resizedImage.toString('base64')}`;
+  return `data:image/png;base64,${resizedImage.toString("base64")}`;
 };
