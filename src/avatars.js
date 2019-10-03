9f89e31b151cfd7e376ecf883793cf26f9698d11
Robin <robin@robin-it.com>
2019-10-03 20:03:15 +0200

    Stop using gh-avatar, as it counts for the API rate limit.

--- src/avatars.js
+++ src/avatars.js
@@ -1,11 +1,10 @@
 "use strict";
 
-import getAvatarURL from "gh-avatar";
 import axios from "axios";
 import sharp from "sharp";
 
 export const fetchEmbedableAvatar = async (username, token, {avatarSize}) => {
-  const url = await getAvatarURL(username, token);
+  const url = `https://github.com/${username.toLowerCase()}.png`;
   const httpResult = await axios.get(url, {responseType: "arraybuffer"});
   const resizedImage = await sharp(Buffer.from(httpResult.data))
     .resize(avatarSize, avatarSize)
