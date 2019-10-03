fe63f386c88137bb9cb5ec57cd0bd5f814f978a0
Robin <robin@robin-it.com>
2019-10-03 20:25:45 +0200

    Be verbose about caching results.
    Uses warn because stdout produces the SVG.

--- src/avatars.js
+++ src/avatars.js
@@ -12,6 +12,7 @@ export const fetchEmbedableAvatar = async (
   const url = `https://github.com/${username.toLowerCase()}.png`;
   const cache = cacheDir ? createCache({path: cacheDir}) : null;
   const httpResult = await got.get(url, {encoding: null, cache});
+  if (!httpResult.fromCache) console.warn("Cache miss for avatar:", username);
   const resizedImage = await sharp(httpResult.body)
     .resize(avatarSize, avatarSize)
     .png()
