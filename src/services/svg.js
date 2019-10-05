76ba658f6f3b06812d8ac48fd705d3ad955a18ab
Robin <robin@robin-it.com>
2019-10-05 23:44:25 +0200

    Separate svg templates from calculation.

--- src/services/svg.js
+++ src/services/svg.js
@@ -19,20 +19,23 @@ export const svgService = {
         posX: (i % usersPerRow) * sizeWithMargin,
         posY: Math.floor(i / usersPerRow) * sizeWithMargin,
       }))
-      .map(
-        ({id, posX, posY, avatarSlug}) =>
-          `<a href="https://github.com/${id}" class="link" target="_blank" id="${id}" aria-label="Visit the profile of ${id} at GitHub">` +
-          `<image x="${posX}" y="${posY}" width="${avatarSize}" height="${avatarSize}" xlink:href="${avatarSlug}" />` +
-          `</a>`
-      );
+      .map(avatarTemplate(avatarSize));
 
-    const w = sizeWithMargin * usersPerRow - margin;
-    const h = Math.ceil(images.length / usersPerRow) * sizeWithMargin - margin;
-    return (
-      `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${w}" height="${h}">\n` +
-      `<style>.link { cursor: pointer; }</style>\n` +
-      images.join("\n") +
-      `\n</svg>`
-    );
+    return contributorWallTemplate({
+      width: sizeWithMargin * usersPerRow - margin,
+      height: Math.ceil(images.length / usersPerRow) * sizeWithMargin - margin,
+      images,
+    });
   },
 };
+
+const avatarTemplate = (avatarSize) => ({id, posX, posY, avatarSlug}) =>
+  `<a href="https://github.com/${id}" class="link" target="_blank" id="${id}" aria-label="Visit the profile of ${id} at GitHub">` +
+  `<image x="${posX}" y="${posY}" width="${avatarSize}" height="${avatarSize}" xlink:href="${avatarSlug}" />` +
+  `</a>`;
+
+const contributorWallTemplate = ({width, height, images}) =>
+  `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${width}" height="${height}">\n` +
+  `<style>.link { cursor: pointer; }</style>\n` +
+  images.join("\n") +
+  `\n</svg>`;
